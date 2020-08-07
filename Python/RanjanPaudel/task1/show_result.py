cdd44452e68e3a197e0e081a02cfb9170f5ef894
Ranjan Paudel <paudeljimrranjan@gmail.com>
2020-08-07 12:32:53 +0545

    Resolve git comments

--- Python/RanjanPaudel/task1/show_result.py
+++ Python/RanjanPaudel/task1/show_result.py
@@ -16,60 +16,74 @@ parser.add_argument("--store",
                     help=STORE_HELP,
                     required=True)
 
+def get_avg_percentage(total, subject_count):
+    percentage = total / subject_count
+
+    return round(percentage, 2)
+
+def get_formatted_date(date_str):
+    date_object = datetime.datetime.strptime(date_str, '%d-%m-%Y')
+    
+    return datetime.datetime.strftime(date_object, '%B %d, %Y')
+
+def check_file_existance(file_name):
+    if not pathlib.Path(file_name).exists():
+        raise SystemExit('The store file does not exist.')
+
+def get_records(file_name):
+    with open(file_name, 'r', newline='') as result_file:
+        result_file_reader = csv.DictReader(result_file)
+
+        return sorted(result_file_reader, key=lambda i: i['name'])
+
+def display_resluts(results):
+    for result in results:
+        print('***************************')
+        print(result['display_str'])
+        print(f"Total Percentage: {get_avg_percentage(result['total_percentage'], len(result['subjects']))}")
+        print('***************************')
+
+def get_display_string(result, is_initital):
+    _str = ""
+
+    if is_initital:
+        _str += f"Name: {result['name']}\nDOB: {get_formatted_date(result['dob'])}\n"
+    
+    _str += f"{result['subject']}:\n\tScore: {result['score']}\n\tTotal: {result['total']}\n\tPercentage: {result['percentage']}\n"
+
+    return _str
+
 if __name__ == "__main__":
     opts = parser.parse_args()
     arg_dict = opts.__dict__
 
-    if not pathlib.Path(arg_dict['store']).exists():
-        raise SystemExit('The store file does not exist.')
+    check_file_existance(arg_dict['store'])
 
+    sorted_result = get_records(arg_dict['store'])
     result_list = []
 
-    with open(arg_dict['store'], 'r', newline='') as result_file:
-        result_file_reader = csv.DictReader(result_file)
-        sorted_result = sorted(result_file_reader, key=lambda i: i['name'])
-        for row in sorted_result:
-            len_of_list = len(result_list)
-            if len_of_list < 1 or row['name'] != result_list[len_of_list - 1]['name']:
-                result_list.append({
-                    "name": row['name'],
-                    "dob": row['dob'],
-                    "subjects": [
-                        {
-                            "name": row['subject'],
-                            "total": int(row['total']),
-                            "score": float(row['score']),
-                            "percentage": float(row['percentage'])
-                        }
-                    ],
-                    "display_str": \
-f"\
-Name: {row['name']}\n\
-DOB: {datetime.datetime.strftime(datetime.datetime.strptime(row['dob'], '%d-%m-%Y'), '%B %d, %Y')}\n\
-{row['subject']}:\n\
-\tScore: {row['score']}\n\
-\tTotal: {row['total']}\n\
-\tPercentage: {row['percentage']}\n",
-                    "total_percentage": float(row['percentage'])
-                })
-            else:
-                subjects_len = len(result_list[len_of_list - 1]['subjects'])
-                result_list[len_of_list-1]['subjects'].append({
-                    "name": row['subject'],
-                    "total": int(row['total']),
-                    "score": float(row['score']),
-                    "percentage": float(row['percentage'])
-                })
-                result_list[len_of_list - 1]['total_percentage'] += float(row['percentage']) 
-                result_list[len_of_list - 1]['display_str'] \
-+= f"\
-{row['subject']}:\n\
-\tScore: {row['score']}\n\
-\tTotal: {row['total']}\n\
-\tPercentage: {row['percentage']}\n"
+    for row in sorted_result:
+        len_of_list = len(result_list)
+        if len_of_list < 1 or row['name'] != result_list[len_of_list - 1]['name']:
+            result_list.append({
+                "name": row['name'],
+                "dob": row['dob'],
+                "subjects": [
+                    {
+                        "name": row['subject'],
+                        "percentage": float(row['percentage'])
+                    }
+                ],
+                "display_str": get_display_string(row, True),
+                "total_percentage": float(row['percentage'])
+            })
+        else:
+            subjects_len = len(result_list[len_of_list - 1]['subjects'])
+            result_list[len_of_list-1]['subjects'].append({
+                "name": row['subject'],
+                "percentage": float(row['percentage'])
+            })
+            result_list[len_of_list - 1]['total_percentage'] += float(row['percentage']) 
+            result_list[len_of_list - 1]['display_str'] += get_display_string(row, False)
 
-        for result in result_list:
-            print('***************************')
-            print(result['display_str'])
-            print(f"Total Percentage: {result['total_percentage'] / len(result['subjects'])}")
-            print('***************************')
+    display_resluts(result_list)
--- Python/RanjanPaudel/task1/store_result.py
+++ Python/RanjanPaudel/task1/store_result.py
@@ -15,14 +15,45 @@ STORE_HELP = "File name to store the result. e.g. store.csv"
 
 FIELD_NAMES = ['name', 'dob', 'subject', 'total', 'score', 'percentage']
 
-
 def validate_date(date_string):
     try:
-        datetime.datetime.strptime(date_string, '%d-%m-%Y')
-        return date_string
+        return f"{datetime.datetime.strptime(date_string, '%d-%m-%Y')}".split(' ')[0]
     except (ValueError):
         raise SystemExit('Invalid date format. Should be DD-MM-YYYY.')
 
+def validate_score(result):
+    if result['score'] > result['total'] or result['score'] < 0 or result['score'] == -0.0:
+        raise SystemExit(
+            'Score not in valid range. \npython3 store_result.py --help, for detail.')
+    
+def validate_file_type(file_name):
+    if file_name.find('.csv') == -1:
+        raise SystemExit('File extension should be .csv')
+
+def create_new_file(file_name):
+    with open(file_name, 'w', newline='') as new_file:
+        new_file_writer = csv.writer(new_file, delimiter=',')
+        new_file_writer.writerow(FIELD_NAMES)
+    print(f"New file {file_name}, created.")
+
+def check_for_duplicate_record(file_name, result):
+    with open(file_name, 'r', newline='') as result_file:
+        result_file_reader = csv.DictReader(result_file)
+        for row in result_file_reader:
+            if row['name'] == result['name'] and row['dob'] == result['dob'] and row['subject'] == result['subject']:
+                raise SystemExit('Result already exists.')
+
+def write_new_record(file_name, result):
+    with open(file_name, 'a', newline='') as result_file:
+        result_file_writer = csv.DictWriter(
+            result_file, fieldnames=FIELD_NAMES)
+        result_file_writer.writerow(result)
+    print(f"New record added:\n{result}")
+
+def get_percentage(result):
+    percentage = (result['score'] / result['total']) * 100
+    
+    return round(percentage, 2)
 
 parser = argparse.ArgumentParser(description=USAGE_EXAMPLE,
                                  formatter_class=argparse.RawDescriptionHelpFormatter)
@@ -52,28 +83,15 @@ if __name__ == "__main__":
     result_dict = opts.__dict__
     file_name = result_dict.pop('store')
 
-    if result_dict['score'] > result_dict['total'] or result_dict['score'] < 0 or result_dict['score'] == -0.0:
-        raise SystemExit(
-            'Score not in valid range. \npython3 store_result.py --help, for detail.')
+    validate_score(result_dict)
+    validate_file_type(file_name)
+
+    result_dict['percentage'] = get_percentage(result_dict)
 
     if not pathlib.Path(file_name).exists():
-        if file_name.find('.csv') == -1:
-            raise SystemExit('File extension should be .csv')
+        create_new_file(file_name)
+        write_new_record(file_name, result_dict)
+        exit()
 
-        with open(file_name, 'w', newline='') as new_file:
-            new_file_writer = csv.writer(new_file, delimiter=',')
-            new_file_writer.writerow(FIELD_NAMES)
-
-    print(result_dict)
-
-    with open(file_name, 'r', newline='') as result_file:
-        result_file_reader = csv.DictReader(result_file)
-        for row in result_file_reader:
-            if row['name'] == result_dict['name'] and row['dob'] == result_dict['dob'] and row['subject'] == result_dict['subject']:
-                raise SystemExit('Result already exists.')
-    result_dict['percentage'] = (
-        result_dict['score'] / result_dict['total']) * 100
-    with open(file_name, 'a', newline='') as result_file:
-        result_file_writer = csv.DictWriter(
-            result_file, fieldnames=FIELD_NAMES)
-        result_file_writer.writerow(result_dict)
+    check_for_duplicate_record(file_name, result_dict)
+    write_new_record(file_name, result_dict)
