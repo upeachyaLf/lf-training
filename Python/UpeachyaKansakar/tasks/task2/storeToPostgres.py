5ca48c86b69eaf36225e4f137185b58bc0041c31
upeachyaLf <upeachyakansakar@lftechnology.com>
2020-08-15 20:16:37 +0545

    storing data to postgres

--- Python/UpeachyaKansakar/tasks/task2/storeToPostgres.py
+++ Python/UpeachyaKansakar/tasks/task2/storeToPostgres.py
@@ -3,12 +3,14 @@ import psycopg2
 
 conn = psycopg2.connect("host=localhost dbname=python_training user=postgresuser password=root")
 cur = conn.cursor()
+
 cur.execute("""
     CREATE TABLE articles(
     title text,
     author text
 )
 """)
+
 with open('output/articles.csv', 'r') as f:
     reader = csv.reader(f)
     # NOTE: for skippin the header row.
@@ -18,4 +20,6 @@ with open('output/articles.csv', 'r') as f:
         "INSERT INTO articles VALUES (%s, %s)",
         row
     )
+
+# NOTE: commit needs to be run at end of our transaction
 conn.commit()
