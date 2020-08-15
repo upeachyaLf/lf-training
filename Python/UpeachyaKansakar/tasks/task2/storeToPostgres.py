960bd6af2bc32281d48ccb5acb216a4d5a52c349
upeachyaLf <upeachyakansakar@lftechnology.com>
2020-08-15 20:13:28 +0545

    storing data to postgres

---
+++ Python/UpeachyaKansakar/tasks/task2/storeToPostgres.py
@@ -0,0 +1,21 @@
+import csv
+import psycopg2
+
+conn = psycopg2.connect("host=localhost dbname=python_training user=postgresuser password=root")
+cur = conn.cursor()
+cur.execute("""
+    CREATE TABLE articles(
+    title text,
+    author text
+)
+""")
+with open('output/articles.csv', 'r') as f:
+    reader = csv.reader(f)
+    # NOTE: for skippin the header row.
+    next(reader) 
+    for row in reader:
+        cur.execute(
+        "INSERT INTO articles VALUES (%s, %s)",
+        row
+    )
+conn.commit()
