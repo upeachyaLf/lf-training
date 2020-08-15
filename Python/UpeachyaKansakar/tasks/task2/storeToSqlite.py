c700cbb6d04a766c42dee367096592b97c7e63e8
upeachyaLf <upeachyakansakar@lftechnology.com>
2020-08-15 21:38:52 +0545

    task2 adding main in sqlite

--- Python/UpeachyaKansakar/tasks/task2/storeToSqlite.py
+++ Python/UpeachyaKansakar/tasks/task2/storeToSqlite.py
@@ -1,35 +1,38 @@
 import csv
-import os
 import sqlite3
 
-# File path.
-filePath = 'output/articles.csv'
-# Database.
-database = '/home/upeachya/training'
+def main():
+    # File path.
+    filePath = 'output/articles.csv'
+    # Database.
+    database = '/home/upeachya/training'
 
-connect = sqlite3.connect(database)
-cursor = connect.cursor()
+    connect = sqlite3.connect(database)
+    cursor = connect.cursor()
 
-# Creating a new table
-cursor.execute("""
-    CREATE TABLE articles(
-    title text,
-    author text
-)
-""")
+    # Creating a new table
+    cursor.execute("""
+        CREATE TABLE articles(
+        title text,
+        author text
+    )
+    """)
 
-# SQL to insert person information.
-sqlInsert = \
-"INSERT INTO articles (title, author)  \
-VALUES (?, ?)"
+    # SQL to insert person information.
+    sqlInsert = \
+    "INSERT INTO articles (title, author)  \
+    VALUES (?, ?)"
 
-with open(filePath, 'r') as f:
-    reader = csv.DictReader(f)
-    # NOTE: for skippin the header row.
-    next(reader) 
-    for row in reader:
-        cursor.execute(sqlInsert, (row['title'],row['authors']))
+    with open(filePath, 'r') as f:
+        reader = csv.DictReader(f)
+        # NOTE: for skippin the header row.
+        next(reader) 
+        for row in reader:
+            cursor.execute(sqlInsert, (row['title'],row['authors']))
 
-connect.commit()
+    connect.commit()
 
-connect.close()
+    connect.close()
+
+if __name__ == "__main__":
+    main()
