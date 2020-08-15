90084c59d109d9594e639707d9812685b363a390
upeachyaLf <upeachyakansakar@lftechnology.com>
2020-08-15 18:18:09 +0545

    task2 save as json

--- Python/UpeachyaKansakar/tasks/task2/scraping.py
+++ Python/UpeachyaKansakar/tasks/task2/scraping.py
@@ -1,5 +1,5 @@
 import time
-import pandas 
+from pandas import DataFrame 
 import requests
 import urllib.request
 from bs4 import BeautifulSoup
@@ -18,8 +18,13 @@ def get_total_pages(soupObject):
     return len(pagination_div.findAll('li'))
 
 def save_as_csv (all_titles):
-    df = pandas.DataFrame({'title':all_titles}) 
+    df = DataFrame({'title':all_titles}) 
     df.to_csv('output/articles.csv', index=False, encoding='utf-8')
+
+def save_as_json (all_titles):
+    data = {'titles':all_titles}
+    df = DataFrame(data, columns= ['titles'])
+    df.to_json('output/articles.json')
 
 def main():
     all_titles = []
@@ -30,14 +35,16 @@ def main():
     # NOTE: looping to scrape all pages
     total_pages = get_total_pages(soup)
     # NOTE: page 0 is already fetched so starting from 1
-    for pageNumber in range(1, total_pages-1):
-        url_with_pagination = URL + '/page/' + str(pageNumber)
-        response = requests.get(url_with_pagination)
-        soup = BeautifulSoup(response.text, 'html.parser') 
-        all_titles = all_titles + scrap_page_title(soup)
-        time.sleep(3)
+    # for pageNumber in range(1, total_pages-1):
+    #     url_with_pagination = URL + '/page/' + str(pageNumber)
+    #     response = requests.get(url_with_pagination)
+    #     soup = BeautifulSoup(response.text, 'html.parser') 
+    #     all_titles = all_titles + scrap_page_title(soup)
+    #     time.sleep(3)
 
-    save_as_csv(all_titles)
+    # save_as_csv(all_titles)
+    save_as_json(all_titles)
+    
     import pdb
     pdb.set_trace()
 
