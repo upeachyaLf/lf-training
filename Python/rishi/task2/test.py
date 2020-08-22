af49ec4f4942d02a48aaea63f7192641178ab75f
jungrishi <ranabhat.85@gmail.com>
2020-08-22 22:59:11 +0545

    Task3:Create a test database and add fixtures

--- Python/rishi/task2/test.py
+++ Python/rishi/task2/test.py
@@ -1,4 +1,7 @@
 import pytest
+import psycopg2
+import os
+import logging
 
 from scrapper import get_filepath_name
 
@@ -8,4 +11,23 @@ def test_get_filepath_name():
 
     result = get_filepath_name(filepath, filename)
     assert result == 'home/lf/rishi/searchfile'
-    
+
+@pytest.fixture(scope='module')
+def cursor(conx):
+    cursor = conx.cursor()
+    logging.info('Created A cursor')
+    yield cursor
+    conx.rollback()
+
+@pytest.fixture
+def test_create_table(cursor):
+    logging.info('Create Table')
+    statement = """create table test_tables(id INT, name varchar(20))"""
+    cursor.execute(statement)
+
+def test_table_exists(cursor, test_create_table):
+    logging.info('Running Test')
+    cursor.execute('select * from test_tables')
+    result = cursor.fetchall()
+
+    assert len(result) == 0
