4518c8d744c413964cd29eaf26bb2d8fb9ddce6e
RungtaPrachi <prachirungta@lftechnology.com>
2020-08-22 20:56:30 +0545

    Added context manager

--- Python/PrachiRungta/task2/storesqlite.py
+++ Python/PrachiRungta/task2/storesqlite.py
@@ -5,21 +5,19 @@ db_filename='bookstore.db'
 data_filename='output/filename.csv'
 conn=sqlite3.connect(db_filename)
 
-cursor=conn.cursor()
-
 #create table
-cursor.execute('''create table if not exists book_stock
-             (book_name text, price real)''')
+with conn:
+    conn.execute('''create table if not exists book_stock
+                 (book_name text, price real)''')
 
-#insert query
-insertbooks=("insert into book_stock(book_name, price)values(:book_name,:price)")
-
-#open the data file and store the data into db
+#open the data file and store the data into db using insert query
 with open(data_filename, 'r') as csvfile:
     reader = csv.DictReader(csvfile)
     next(reader) 
     for row in reader:
-        cursor.execute(insertbooks, (row['book_name'],row['price']))   
+        with conn:
+            conn.execute(("insert into book_stock(book_name, price)values(:book_name,:price)"), 
+            (row['book_name'],row['price']))  
 
 #select table data from book_stock
 for row in conn.execute("select book_name, price from book_stock"):
